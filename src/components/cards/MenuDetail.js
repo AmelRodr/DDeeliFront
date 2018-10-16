120bd955464e723a4c352d1c796c1e49fe322c0a
AmelRodr <amelrodr@gmail.com>
2018-10-15 22:11:26 -0500

    columcenterdes

--- src/components/cards/MenuDetail.js
+++ src/components/cards/MenuDetail.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react'
 import Nab from '../home/Nab';
 import Foter from '../home/Foter';
-// import { storiesOf } from '@storybook/react';
-
-// import Pagination from 'react-bulma-components/lib/components/pagination';
+import { Grid,Container } from 'semantic-ui-react'
 import { Layout} from 'antd';
 import MenuFeed from './MenuFeed';
+import MenuDescription from './MenuDescription';
 const { Content } = Layout;
-// storiesOf('Pagination', module)
-//   .addDecorator(story => <div style={{ margin: 50 }}>{story()}</div>)
-//   .add('Default', () => <Pagination current={3} total={5} delta={1} />)
+
 class MenuDetail extends Component {
     render() {
         return (
             <div>
-
-                 <MenuFeed/>
                 <Layout className="layout">
                 <Nab/>
+            
+
+
                     <Content style={{ padding: '0 10px' }}>
-                   
-                     <h1>Detalle</h1>
-                    
-                    
+                        <Grid centered columns={3}>
+                            <Grid.Column width={12}>
+                                <Container>
+                                    <div style={{width:'100px', height:'100px'}}>
+                                        hola
+                                    </div>
+                                    <MenuDescription />
+                                    <MenuFeed />
+                                </Container>
+                            </Grid.Column>
+                        </Grid>
                     </Content>
+                    
+              
+
+                    
                     <Foter/>
                 </Layout>               
             </div>
