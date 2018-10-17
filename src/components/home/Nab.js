902fcd7f0d7b7ae5ccc524f3216d843ecce17bf2
AmelRodr <amelrodr@gmail.com>
2018-10-17 04:20:17 -0500

    todos los botones funcionando al 100

--- src/components/home/Nab.js
+++ src/components/home/Nab.js
@@ -2,72 +2,68 @@
 import React, { Component } from 'react';
 import logo from '../../logo.png'
 import Login from '../auth/Login';
-import { Collapse,
+import {
+    Collapse,
     Navbar,
     NavbarToggler,
     NavbarBrand,
     Nav,
     NavItem,
-    Button } from 'reactstrap';
-import  {Link} from 'react-router-dom'
+    Button
+} from 'reactstrap';
+import { Link } from 'react-router-dom'
 
 class Nab extends Component {
-  constructor(props) {
+    constructor(props) {
         super(props);
-    
+
         this.toggle = this.toggle.bind(this);
         this.state = {
-          isOpen: false
+            isOpen: false
         };
-      }
-      toggle (){
+    }
+    toggle() {
         this.setState({
-          isOpen: !this.state.isOpen
+            isOpen: !this.state.isOpen
         });
-      }
+    }
 
-      logout= ()=> {
-          localStorage.removeItem('token')
-          localStorage.removeItem('user')
-         
-       }
-    //   onOurMenu= ()=> {
-    //       this.props.history.push('/ourmenu')
-    //   }
-    //   onSignup = () =>{
-    //     this.props.history.push('/signup')
-    // }
-    
+    logout = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+
+    }
+
     render() {
-    console.log(this.props)
-    const islogged = localStorage.getItem('token')
-    console.log('isloged',islogged)
+        console.log(this.props)
+        const islogged = localStorage.getItem('token')
+        console.log('isloged', islogged)
         return (
             <div>
                 <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/"> <img src={logo} width='130' style={{ borderRadius: '10%' }} alt='' /> </NavbarBrand>
+                <Link to='/' > <NavbarBrand> <img src={logo} width='130' style={{ borderRadius: '10%' }} alt='' /> </NavbarBrand></Link>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
 
                         <Nav className="ml-auto" navbar>
                             <NavItem>
                                 <div style={{ marginRight: '50px' }}>
-                                   {!islogged? '': <Link to='/ourmenu' ><Button  outline color="success">Nuestro Menu</Button>  </Link>}
+                                    {!islogged ? '' : <Link to='/ourmenu' ><Button outline color="success">Nuestro Menu</Button>  </Link>}
                                 </div>
                             </NavItem>
                             <NavItem>
                                 <div style={{ marginRight: '50px' }}>
-                                   {!islogged?  <Link to='/ourmenu' ><Button Linkto='/ourmenu' outline color="success">our Menu</Button> </Link> : <Button Linkto='/ourmenu' outline color="success">Quieres ser chef?</Button> } 
+                                    {!islogged ? <Link to='/ourmenu' ><Button outline color="success">our Menu</Button> </Link> :<Link to='/ourmenu' > <Button outline color="success">Quieres ser chef?</Button></Link>}
                                 </div>
                             </NavItem>
                             <NavItem>
                                 <div style={{ marginRight: '5px' }}>
-                                {!islogged ? <Login {...this.props} /> :<Button href='/profile' color="info" >Profile</Button>  }
-                                
+                                    {!islogged ? <Login {...this.props} /> :<Link to='/profile' > <Button  color="info" >Profile</Button></Link>}
+
                                 </div>
                             </NavItem>
                             <NavItem>
-                              { !islogged ? <Link to='/signup' ><Button color="info" >Signup</Button></Link>  : <Button href='/' onClick={this.logout} color="danger" >Logout</Button> }   
+                                {!islogged ? <Link to='/signup' ><Button color="info" >Signup</Button></Link> :<Link to='/' >  <Button href='/' onClick={this.logout} color="danger" >Logout</Button></Link>}
                             </NavItem>
 
                         </Nav>
